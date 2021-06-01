efc0d8dd622c0c868a86ac0674a04c9cf1baa441
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-06-01 18:35:01 -0300

    sacamos los casos de prueba

--- Acero/src/main/java/mainDea.java
+++ Acero/src/main/java/mainDea.java
@@ -1,26 +1,18 @@
 
 import views.ventana;
-import business.Cliente;
-import business.Envase;
-import business.Articulo;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class mainDea {
 
     private static ventana unaVentana = new ventana();
-    private static Cliente unCliente = new Cliente(1);
-    private static Envase unEnvase = new Envase(1, 1);
-    private static List<Envase> listaEnvases = new ArrayList<>();
+
 
 
     
 
     public static void main(String[] args) {
 
-        listaEnvases.add(unEnvase);
-        Articulo unArticulo = new Articulo(1,listaEnvases);
+
         unaVentana.mostrarMenu();
 
 
